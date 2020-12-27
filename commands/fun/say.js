cfc443df1b6c4ac2c7427c060095ec838f80480b
Tomato#6966 <68145571+Tomato6966@users.noreply.github.com>
2020-12-27 19:25:26 +0100

    Add files via upload

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -1,10 +1,17 @@
-
+//Here the command starts
 module.exports = {
-	name: "say",
-	category: "fun",
-	aliases: ["say"],
-  description: "Resends the message",
-  run: async (client, message, args) => {
-	message.channel.send(args.join(" "))
+	//definition
+	name: "say", //the name of the command 
+	category: "fun", //the category this will be listed at, for the help cmd
+	aliases: ["say", "sayit"], //every parameter can be an alias
+	cooldown: 4, //this will set it to a 4 second cooldown
+	usage: "say <Text>", //this is for the help command for EACH cmd
+  	description: "Resends the message", //the description of the command
+
+	//running the command with the parameters: client, message, args, user, text, prefix
+  run: async (client, message, args, user, text, prefix) => {
+		//EVERTHING in HERE CAN BE A PART OF THE COMMAND	
+		message.channel.send(text) //you could also do:  message.channel.send(args.join(" "))
+		//another example: message.channel.send(user + "send the message: " + text)
 	}
 }
--- commands/info/help.js
+++ commands/info/help.js
@@ -1,53 +1,63 @@
 const { MessageEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
-const config = require("../../config.json");
+const config = require("../../botconfig/config.json");
+const color = require("../../botconfig/colors.json");
+//Here the command starts
 module.exports = {
-    name: "help",
-    aliases: ["h", "cmds"],
-    category: "info",
-    description: "Returns all Commands, or one specific Command information",
-    usage: "[command], [aliases], [command <cmd>]",
-    run: async (client,message,args) => {
-        if(args[0]){
+    //definition
+     name: "help", //the name of the command 
+     category: "info", //the category this will be listed at, for the help cmd
+     aliases: ["h", "commandinfo"], //every parameter can be an alias or empty for no aliases
+     cooldown: 5, //this will set it to a 5 second cooldown
+     usage: "help [Command]", //this is for the help command for EACH cmd
+     description: "Returns all Commmands, or one specific command", //the description of the command
+ 
+     //running the command with the parameters: client, message, args, user, text, prefix
+   run: async (client, message, args, user, text, prefix) => {
+        if(args[0]){ //if there are arguments then get the command help
             return getCMD(client,message,args[0]);
         }
-        else{
+        else{ //if not get all commands
             return getAll(client, message);
         }
     }
 }
-
+//function for getting all commands
 function getAll(client,message){
-const embed = new MessageEmbed()
-    .setColor("ORANGE")
+const embed = new MessageEmbed() //defining the Embed
+    .setColor(color.main)
     .setThumbnail(client.user.displayAvatarURL())
     .setTitle("HELP MENU")
-    .addField("**__BOT BY:__**", `
-    >>> <@442355791412854784> \`Tomato#6966\` [\`Website\`](https://musicium.eu)
-    `)
     .setFooter(`TO see command descriptions and inforamtion, type: ${config.prefix}help [CMD NAME]`, client.user.displayAvatarURL())
-    const commands = (category) => {
+    const commands = (category) => { //finding all commands and listing them into a string with filter and map
         return client.commands.filter(cmd => cmd.category === category)
                 .map(cmd => `\`${cmd.name}\``).join(", ")
     }
+    //get the command infostring
     const info = client.categories.map(cat => stripIndents`**__${cat[0].toUpperCase() + cat.slice(1)}__**\n> ${commands(cat)}`)
     .reduce((string, category) => string + "\n" + category);
+    //sending the embed with the description
     return message.channel.send(embed.setDescription(info))
 }
+//function for all commands
 function getCMD(client,message,input){
-    const embed = new MessageEmbed()
-    const cmd = client.commands.get(input.toLowerCase()) || client.commands.get(client.aliases.get(input.toLowerCase()))
-    let info = `No Information found for command **${input.toLowerCase()}**`;
-    if(!cmd){
-        return message.channel.send(embed.setColor("RED").setDescription(info));
+    const embed = new MessageEmbed() //creating a new Embed
+
+    const cmd = client.commands.get(input.toLowerCase()) || client.commands.get(client.aliases.get(input.toLowerCase())) //getting the command by name/alias
+    if(!cmd){ //if no cmd found return info no infos!
+        return message.channel.send(embed.setColor(color.no).setDescription(`No Information found for command **${input.toLowerCase()}**`));
     }
-    info = "";
-    if(cmd.name) info += `**Command name**: \`${cmd.name}\`\n`
-    if(cmd.aliases) info += `**Aliases**: \`${cmd.aliases.map(a => `${a}`).join("\`, \`")}\`\n`
-    if(cmd.description) info += `**Description**: \`${cmd.description}\`\n`
-    if(cmd.usage) {
-        info += `\n**Usage**: ${cmd.usage}`;
+    if(cmd.name) embed.addField("**Command name**", `\`${cmd.name}\``)
+    if(cmd.name) embed.setTitle(`Detailed Information about: \`${cmd.name}\``)
+    if(cmd.description) embed.addField("**Description**", `\`${cmd.description}\``);
+
+    if(cmd.aliases) embed.addField("**Aliases**", `\`${cmd.aliases.map(a => `${a}`).join("\`, \`")}\``)
+    if(cmd.cooldown) embed.addField("**Cooldown**", `\`${cmd.cooldown} Seconds\``)
+        else embed.addField("**Cooldown**", `\`1 Second\``)
+    if(cmd.usage){
+        embed.addField("**Usage**", `\`${config.prefix}${cmd.usage}\``);
         embed.setFooter("Syntax: <> = required, [] = optional"); 
     }
-    return message.channel.send(embed.setColor("ORANGE").setDescription(info))
+    //send the new Embed
+    return message.channel.send(embed.setColor(color.main))
 }
