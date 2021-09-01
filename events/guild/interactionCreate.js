75d50f085d7f0bb3f7028c612f69cecbe540e40d
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-09-01 18:29:48 +0200

    Update interactionCreate.js

--- events/guild/interactionCreate.js
+++ events/guild/interactionCreate.js
@@ -7,11 +7,14 @@ const Discord = require("discord.js");
 module.exports = (client, interaction) => {
 	const CategoryName = interaction.commandName;
 	let command = false;
-	if (client.slashCommands.has(CategoryName + (interaction.options && interaction.options.length > 0 ? interaction.options.getSubcommand() : interaction.commandName))) {
-    		command = client.slashCommands.get(CategoryName + (interaction.options && interaction.options.length > 0 ? interaction.options.getSubcommand() : interaction.commandName));
-	}
-	if (client.slashCommands.has("normal" + CategoryName)) {
-		command = client.slashCommands.get("normal" + CategoryName);
+	try{
+    	    if (client.slashCommands.has(CategoryName + interaction.options.getSubcommand())) {
+      		command = client.slashCommands.get(CategoryName + interaction.options.getSubcommand());
+    	    }
+  	}catch{
+    	    if (client.slashCommands.has("normal" + CategoryName)) {
+      		command = client.slashCommands.get("normal" + CategoryName);
+   	    }
 	}
 	if(command) {
 		if (onCoolDown(interaction, command)) {
